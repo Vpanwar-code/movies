66b9e39df7bdc3c2dc86f0b0bedb13ddaa8c4ade
Vpanwar-code <vivekpanwar183@gmail.com>
2023-07-01 19:15:08 +0530

    adding get api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
-
+import { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies,setMovies]=useState([]);
+  async function fetchMoviesHandler(){
+
+    const response=await fetch('https://swapi.dev/api/films');
+    const data=await response.json();
+
+    const transformedmovies = data.results.map(movieData =>{
+      return{
+        id : movieData.spisode_id,
+        title : movieData.title,
+        openingText : movieData.opening_crawl,
+        releaseDate : movieData.release_date,
+      };
+    });
+    console.log(transformedmovies);
+    setMovies(transformedmovies);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
