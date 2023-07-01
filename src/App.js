2039333c1044203a337e5a5121b895f016c810e8
Vpanwar-code <vivekpanwar183@gmail.com>
2023-07-01 22:37:18 +0530

    adding loading feature

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import './App.css';
 
 function App() {
   const [movies,setMovies]=useState([]);
+  const [isLoading, setIsLoading]=useState(false);
   async function fetchMoviesHandler(){
-
+    setIsLoading(true);
     const response=await fetch('https://swapi.dev/api/films');
     const data=await response.json();
-
+     
     const transformedmovies = data.results.map(movieData =>{
       return{
         id : movieData.spisode_id,
@@ -20,6 +21,7 @@ function App() {
     });
     console.log(transformedmovies);
     setMovies(transformedmovies);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
